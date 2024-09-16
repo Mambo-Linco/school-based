f24b20ec2b224c020034bb4ce30c1ad916d7f551
Mambo-Linco <166580269+Mambo-Linco@users.noreply.github.com>
2024-09-16 15:18:43 +0200

    Add files via upload

---
+++ registration/login_form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Form</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="form-container">
+        <form action="" method="post">
+            <h3>Login now</h3>
+            <input type="text " name="name" required placeholder="enter your name">
+            <input type="email " name="email" required placeholder="enter your email">
+            <input type="password " name="password" required placeholder="enter your password">
+            <input type="password " name="password" required placeholder="confirm your password">
+            <p>Don't have an account? <a href="register_form.php">Login</p>
+
+
+
+
+
+        </form>
+
+    </div>
+    
+</body>
+</html>
